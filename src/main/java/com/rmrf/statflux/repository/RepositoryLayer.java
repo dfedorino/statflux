64a294aa05a227a176a9ebd692cd29f7170401f7
Alexey Goloshchapov <april1889@gmail.com>
2026-04-17 19:37:34 +0300

    RepositoryLayer: add comments

--- src/main/java/com/rmrf/statflux/repository/RepositoryLayer.java
+++ src/main/java/com/rmrf/statflux/repository/RepositoryLayer.java
@@ -7,18 +7,40 @@ import lombok.NonNull;
 
 public interface RepositoryLayer {
 
+    /**
+     * @param hostingName идентификатор хостинга
+     * @param rawLink     необработанный url видео
+     * @param hostingId   идентификатор видео по данным хостинга
+     * @param name        заголовок видео
+     * @param views       количество просмотров
+     * @return true - операция успешна, false - нет
+     */
     boolean save(@NonNull String hostingName, @NonNull String rawLink, @NonNull String hostingId,
         @NonNull String name, Long views);
 
+    /**
+     * @param skip пагинация - сколько документов пропустить
+     * @param take пагинация - сколько документов вернуть на одной странице
+     * @return данные, отсортированные
+     */
     @NonNull
     List<VideoStatsItem> getVideos(Optional<String> skip, Optional<Integer> take);
 
+    /**
+     * @return данные всех сохранённых видео
+     */
     @NonNull
     default List<VideoStatsItem> getVideos() {
         return getVideos(Optional.empty(), Optional.empty());
     }
 
+    /**
+     * @return суммарное количество видео в базе
+     */
     int getTotalVideosCount();
 
+    /**
+     * @return суммарное количество просмотров всех видео в базе
+     */
     long getTotalViewCount();
 }
