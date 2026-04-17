632e85019149c24c14374758ed2859dd7086fbfa
cd9cc <32744785+cd9cc@users.noreply.github.com>
2026-04-17 19:44:15 +0300

    Update src/main/java/com/rmrf/statflux/service/ServiceLayerImpl.java
    Co-authored-by: dfedorino <dfedorino@gmail.com>

--- src/main/java/com/rmrf/statflux/service/ServiceLayerImpl.java
+++ src/main/java/com/rmrf/statflux/service/ServiceLayerImpl.java
@@ -17,6 +17,7 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 public class ServiceLayerImpl implements ServiceLayer {
 
     private final RepositoryLayer repositoryLayer;
