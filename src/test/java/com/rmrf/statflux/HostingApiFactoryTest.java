85ffc976f6876813e39a34440b743ab8a576d0c6
Alexey Goloshchapov <april1889@gmail.com>
2026-04-17 19:43:36 +0300

    refactor tests

--- src/test/java/com/rmrf/statflux/HostingApiFactoryTest.java
+++ src/test/java/com/rmrf/statflux/HostingApiFactoryTest.java
@@ -1,79 +1,52 @@
 package com.rmrf.statflux;
 
+import static com.rmrf.statflux.Stubs.vkStub;
+import static com.rmrf.statflux.Stubs.youtubeStub;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.rmrf.statflux.domain.dto.LinkMetadataResponse;
 import com.rmrf.statflux.domain.exceptions.BadUrlException;
 import com.rmrf.statflux.domain.exceptions.UnsupportedUrlException;
 import com.rmrf.statflux.integration.HostingApiFactory;
-import com.rmrf.statflux.integration.vk.VkHostingApi;
-import com.rmrf.statflux.integration.youtube.YouTubeHostingApi;
-import com.rmrf.statflux.util.result.Result;
-import lombok.NonNull;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class HostingApiFactoryTest {
 
-    private static final YouTubeHostingApi youtubeStub = new YouTubeHostingApi() {
-        @Override
-        public @NonNull Result<LinkMetadataResponse> linkMetadata(String rawLink) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public @NonNull String hostingName() {
-            throw new UnsupportedOperationException();
-        }
-    };
-
-    private static final VkHostingApi vkStub = new VkHostingApi() {
-        @Override
-        public @NonNull Result<LinkMetadataResponse> linkMetadata(String rawLink) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public @NonNull String hostingName() {
-            throw new UnsupportedOperationException();
-        }
-    };
-
     private final HostingApiFactory factory = new HostingApiFactory(youtubeStub, vkStub);
 
-    @Test
-    void returnsYouTubeHostingApiForCorrectYouTubeLinks() {
-        var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("youtubeUrls")
+    void returnsYouTubeHostingApiForCorrectYouTubeLinks(String description, String url) {
         assertThat(factory.forUrl(url).contains(youtubeStub));
     }
 
-    @Test
-    void returnsYouTubeHostingApiForCorrectYouTubeLinksMobile() {
-        var url = "https://m.youtube.com/watch?v=dQw4w9WgXcQ";
-        assertThat(factory.forUrl(url).contains(youtubeStub));
+    private static Stream<Arguments> youtubeUrls() {
+        return Stream.of(
+            Arguments.of("correct YouTube link", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"),
+            Arguments.of("correct mobile YouTube link",
+                "https://m.youtube.com/watch?v=dQw4w9WgXcQ"),
+            Arguments.of("correct shared YouTube link",
+                "https://youtu.be/dQw4w9WgXcQ?si=8ELD_NCsKOPzcfJ0")
+        );
     }
 
-    @Test
-    void returnsYouTubeHostingApiForCorrectYouTubeLinksShare() {
-        var url = "https://youtu.be/dQw4w9WgXcQ?si=8ELD_NCsKOPzcfJ0";
-        assertThat(factory.forUrl(url).contains(youtubeStub));
-    }
-
-    @Test
-    void returnsVkHostingApiForCorrectVkLinks() {
-        var url = "https://vkvideo.ru/video-213638597_456239147";
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("vkUrls")
+    void returnsVkHostingApiForCorrectVkLinks(String description, String url) {
         assertThat(factory.forUrl(url).contains(vkStub));
     }
 
-    @Test
-    void returnsVkHostingApiForCorrectVkLinksMobile() {
-        var url = "https://m.vkvideo.ru/video-213638597_456239147?from=search";
-        assertThat(factory.forUrl(url).contains(vkStub));
-    }
-
-    @Test
-    void returnsVkHostingApiForCorrectVkLinksShare() {
-        var url = "https://vkvideo.ru/video-213638597_456239147?list=ln-ZJlWY456pSfIkF5EYt";
-        assertThat(factory.forUrl(url).contains(vkStub));
+    private static Stream<Arguments> vkUrls() {
+        return Stream.of(
+            Arguments.of("correct VK Video link", "https://vkvideo.ru/video-213638597_456239147"),
+            Arguments.of("correct mobileVK Video link",
+                "https://m.vkvideo.ru/video-213638597_456239147?from=search"),
+            Arguments.of("correct shared VK Video link",
+                "https://vkvideo.ru/video-213638597_456239147?list=ln-ZJlWY456pSfIkF5EYt")
+        );
     }
 
     @Test
---
+++ src/test/java/com/rmrf/statflux/Stubs.java
@@ -0,0 +1,34 @@
+package com.rmrf.statflux;
+
+import com.rmrf.statflux.domain.dto.LinkMetadataResponse;
+import com.rmrf.statflux.integration.vk.VkHostingApi;
+import com.rmrf.statflux.integration.youtube.YouTubeHostingApi;
+import com.rmrf.statflux.util.result.Result;
+import lombok.NonNull;
+
+public class Stubs {
+
+    protected static final YouTubeHostingApi youtubeStub = new YouTubeHostingApi() {
+        @Override
+        public @NonNull Result<LinkMetadataResponse> linkMetadata(String rawLink) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public @NonNull String hostingName() {
+            throw new UnsupportedOperationException();
+        }
+    };
+
+    protected static final VkHostingApi vkStub = new VkHostingApi() {
+        @Override
+        public @NonNull Result<LinkMetadataResponse> linkMetadata(String rawLink) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public @NonNull String hostingName() {
+            throw new UnsupportedOperationException();
+        }
+    };
+}
