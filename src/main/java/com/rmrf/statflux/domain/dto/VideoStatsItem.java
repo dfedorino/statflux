01b7606ca3604cf1b65ac56af190f1a15ed8627c
Alexey Goloshchapov <april1889@gmail.com>
2026-04-17 17:55:44 +0300

    VideoStatsItem: add rawUrl

--- src/main/java/com/rmrf/statflux/domain/dto/VideoStatsItem.java
+++ src/main/java/com/rmrf/statflux/domain/dto/VideoStatsItem.java
@@ -3,7 +3,8 @@ package com.rmrf.statflux.domain.dto;
 import java.time.ZonedDateTime;
 import lombok.NonNull;
 
-public record VideoStatsItem(@NonNull String id, @NonNull String name, @NonNull Long views,
+public record VideoStatsItem(@NonNull String id, @NonNull String name, @NonNull String rawUrl,
+                             @NonNull Long views,
                              @NonNull ZonedDateTime updatedAt) {
 
 }
