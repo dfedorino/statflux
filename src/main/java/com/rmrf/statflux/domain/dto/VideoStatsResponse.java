575d895c00e0b5dd79709e836156c6c7277e7884
Alexey Goloshchapov <april1889@gmail.com>
2026-04-18 19:56:34 +0300

    VideoStatsResponse: rename VideoStatsResponse.hasMore to hasNext

--- src/main/java/com/rmrf/statflux/domain/dto/VideoStatsResponse.java
+++ src/main/java/com/rmrf/statflux/domain/dto/VideoStatsResponse.java
@@ -3,7 +3,13 @@ package com.rmrf.statflux.domain.dto;
 import java.util.List;
 import lombok.NonNull;
 
+/**
+ * @param items       список видео
+ * @param totalVideos общее количество всех видео в базе
+ * @param hasNext     есть ли следующая страница
+ * @param totalViews  общее количество просмотром всех видео в базе
+ */
 public record VideoStatsResponse(@NonNull List<VideoStatsItem> items, int totalVideos,
-                                 boolean hasMore, long totalViews) {
+                                 boolean hasNext, long totalViews) {
 
 }
