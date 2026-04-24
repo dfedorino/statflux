b5c3be1043564495e32a8328077da4d6a94bb1cf
Alexey Goloshchapov <april1889@gmail.com>
2026-04-24 11:23:27 +0300

    TelegramBotFormatter: add more characters for replacement

--- src/main/java/com/rmrf/statflux/bot/infra/util/TelegramBotFormatter.java
+++ src/main/java/com/rmrf/statflux/bot/infra/util/TelegramBotFormatter.java
@@ -1,13 +1,19 @@
 package com.rmrf.statflux.bot.infra.util;
 
 public class TelegramBotFormatter {
+
     /**
      * Экранирование спец. символов для тг
      */
     public static String escapeSpecial(String text) {
         return text.replace(".", "\\.")
-                .replace("!", "\\!")
-                .replace("=", "\\=")
-                .replace("-", "\\-");
+            .replace("!", "\\!")
+            .replace("=", "\\=")
+            .replace("-", "\\-")
+            .replace("+", "\\+")
+            .replace("(", "\\(")
+            .replace(")", "\\)")
+            .replace(":", "\\:")
+            .replace("_", "\\_");
     }
 }
